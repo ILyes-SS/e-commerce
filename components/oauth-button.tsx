c16b4c555656ded389089608609f143c63df0db7
ilyes benmalti <benmaltiilyes@gmail.com>
2025-10-18 19:21:57 +0200

    feat: Add oAuth with google and github

---
+++ components/oauth-button.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { createClient } from "@/lib/supabase/client";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Provider } from "@supabase/supabase-js";
+
+export function OAuthButton({
+  className,
+  provider,
+}: React.ComponentPropsWithoutRef<"div"> & {
+  provider: Provider;
+}) {
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSocialLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const supabase = createClient();
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: provider,
+        options: {
+          redirectTo: `${window.location.origin}/auth/oauth?next=/protected`,
+        },
+      });
+
+      if (error) throw error;
+    } catch (error: unknown) {
+      setError(error instanceof Error ? error.message : "An error occurred");
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSocialLogin} className="mb-2">
+      <div className="flex flex-col gap-6">
+        {error && <p className="text-sm text-destructive-500">{error}</p>}
+        <Button variant="outline" type="submit" className="w-full cursor-pointer" disabled={isLoading}>
+          {isLoading ? "Logging in..." : `Continue with ${provider}`}
+        </Button>
+      </div>
+    </form>
+  );
+}
